abfeeb272626e7245ea16c235430e246278de770
Eshan <eshan.anand10@gmail.com>
2017-10-18 11:56:18 +0530

    1. Main Activity layout defined.2.  Dependency Added in Gradle - ObjectBox, RecyclerView and ButterKnife

--- app/src/main/java/com/hacknews/hacknews/MainActivity.java
+++ app/src/main/java/com/hacknews/hacknews/MainActivity.java
@@ -2,9 +2,24 @@ package com.hacknews.hacknews;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity
 {
+
+    @BindView(R.id.etSearch)
+    EditText etSearch;
+    @BindView(R.id.btnSearch)
+    Button btnSearch;
+    @BindView(R.id.newsRecycler)
+    RecyclerView newsRecycler;
+    @BindView(R.id.progressbar)
+    ProgressBar progressbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
