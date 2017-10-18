66c709b22004fcd62afffaf9ef2129d79b47ac8e
Eshan <eshan.anand10@gmail.com>
2017-10-18 12:49:33 +0530

    btn Search to perform Api Get task and return onEventMainThread in MainActivity

--- app/src/main/java/com/hacknews/hacknews/activities/MainActivity.java
+++ app/src/main/java/com/hacknews/hacknews/activities/MainActivity.java
@@ -2,25 +2,43 @@ package com.hacknews.hacknews.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.hacknews.hacknews.R;
+import com.hacknews.hacknews.apitask.GetApiDataTask;
+import com.hacknews.hacknews.application.App;
+import com.hacknews.hacknews.models.Hit;
+import com.hacknews.hacknews.models.News;
 import com.hacknews.hacknews.models.UpdateNewEvent;
 import com.hacknews.hacknews.util.Logger;
+import com.hacknews.hacknews.util.SharedPref;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import de.greenrobot.event.EventBus;
+import io.objectbox.Box;
+import io.objectbox.BoxStore;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
 
     private static final String TAG = MainActivity.class.getSimpleName();
     private long newsId;
+    private BoxStore boxStore;
+    private Box<News> newsBox;
+    private News news;
+    private SharedPref mSharedPref;
+    private GetApiDataTask getApiDataTask;
+    private List<Hit> hitList = new ArrayList<>();
 
     @BindView(R.id.etSearch)
     EditText etSearch;
@@ -38,6 +56,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         EventBus.getDefault().register(this);
+        boxStore = ((App)this.getApplication()).getBoxStore();
+        newsBox = boxStore.boxFor(News.class);
+        mSharedPref = SharedPref.getInstance(this);
+        newsId = mSharedPref.getNewsId();
+        btnSearch.setOnClickListener(this);
     }
 
     @Override
@@ -52,6 +75,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // your implementation
         Logger.d("LoginINAct", " inside on Event received");
         newsId = event.getMessage();
+
+        if (newsId > 0) {
+            news = newsBox.get(newsId);
+            mSharedPref.saveNewsId(newsId);
+
+            for (int i = 0; i < news.hits.size(); i++)
+            {
+                hitList.add(news.hits.get(i));
+            }
+
+        } else {
+            Toast.makeText(MainActivity.this, "No News present", Toast.LENGTH_LONG).show();
+
+            return;
+        }
     }
 
     @Override
@@ -61,7 +99,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (id)
         {
             case R.id.btnSearch:
-
+                getApiDataTask = new GetApiDataTask(MainActivity.this, etSearch.getText().toString());
+                getApiDataTask.getNewsData();
                 break;
         }
     }
